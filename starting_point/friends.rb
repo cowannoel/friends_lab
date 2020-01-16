039a9c3113a83da3c859ec1c676c8e8d29337b39
roosap <roosa.paivansalo@gmail.com>
2020-01-16 14:26:22 +0000

    Question 3 solution

--- starting_point/friends.rb
+++ starting_point/friends.rb
@@ -5,3 +5,12 @@ end
 def get_tv_show(person)
   return person[:favourites][:tv_show]
 end
+
+def likes_to_eat(person, snack_to_find)
+  for snack in person[:favourites][:snacks]
+    if snack == snack_to_find
+      return true
+    end
+  end
+  return false
+end
